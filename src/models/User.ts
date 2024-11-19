4100af9c95587310f015776de38d1e91adb631a6
Alessandro Centanni <alessandro.centanni1@gmail.com>
2024-11-19 23:28:35 +0100

    hotfix: somehow i didn't make the email unique, lol.

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,7 +15,7 @@ const UserSchema = new Schema<IUser>(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false },
     permissions: { type: [String], default: [] },
   },
